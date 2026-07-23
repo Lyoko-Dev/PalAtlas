716c95e0a134f9ed2d3c990dc920caf7d2e17aa5
Lyoko Dev <dev@lyoko.dev>
2026-07-23 12:38:24 +0200

    Invalidate cached shell after local migration

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,7 @@
-const CACHE = "palatlas-v3";
+const CACHE = "palatlas-v4";
 const ASSETS = ["/", "/index.html", "/web/styles.css", "/web/app.js", "/src/index.js", "/src/data.js", "/src/tools.js", "/src/news.js", "/src/storage.js"];
 self.addEventListener("install", (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS))));
 self.addEventListener("fetch", (event) => event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request))));
+
 
 
