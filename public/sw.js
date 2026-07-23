65897246613a0fc6f5be8d0ccbbd7297b3179961
Lyoko Dev <dev@lyoko.dev>
2026-07-23 13:00:44 +0200

    Use supplied map image and fix image delivery

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,8 @@
-const CACHE = "palatlas-v8";
+const CACHE = "palatlas-v9";
 const ASSETS = ["/", "/index.html", "/web/styles.css", "/web/app.js", "/src/index.js", "/src/data.js", "/src/tools.js", "/src/news.js", "/src/storage.js"];
 self.addEventListener("install", (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS))));
 self.addEventListener("fetch", (event) => event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request))));
+
 
 
 
